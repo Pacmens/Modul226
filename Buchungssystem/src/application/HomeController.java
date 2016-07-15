18739e4563d9170aa556b2d6cd6ad1eec0069b72
Linus Eberhard <leberhard@gmx.ch>
2016-07-15 08:20:48 +0200

    ànderungen an HomeController Budget

--- Buchungssystem/src/application/HomeController.java
+++ Buchungssystem/src/application/HomeController.java
@@ -1,7 +1,10 @@
 package application;
 
+import database.DatabaseManager;
+import entities.Budget;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import mapper.DatabaseMapper;
 
 public class HomeController {
 
@@ -10,27 +13,29 @@ public class HomeController {
 	@FXML
 	private Label lbKontostand;
 	private Double budget;
-	
+
 	@FXML
-	protected void initialize() {
+	protected void initialize() throws Exception {
 		String sql = "Select * From BUDGET WHERE PERSON_ID = '" + Main.getPersonId() + "'";
+		Budget budget = (Budget) DatabaseMapper.getObjectOfResutSet("/mapping/mapBudget.standartmapBudget", DatabaseManager.getDatabaseManager().sendQuery(sql));
+
 		// Main.getBudgetList() = SQL-Output
 		calculateBudget();
 		lbKontostand.setText(budget.toString());
 		lbWelcomeText.setText("Wilkommen " + Main.getName() + ", Sie sind eingeloggt.");
 	}
-	
+
 	private void  calculateBudget() {
 		budget = 0.0;
 	}
-	
+
 	public Label getLbWelcomeText() {
 		return lbWelcomeText;
 	}
 	public void setLbWelcomeText(Label lbWelcomeText) {
 		this.lbWelcomeText = lbWelcomeText;
 	}
-	
+
 	public Label getLbKontostand() {
 		return lbKontostand;
 	}
